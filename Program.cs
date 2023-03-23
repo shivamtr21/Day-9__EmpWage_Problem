36ffe32f817e88d8d745d624ea30e98d7b1c35d2
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-23 20:41:39 +0530

    Check wage using oops concept

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineProblemUseObject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeWage_UC7 UC7 = new EmployeeWage_UC7();
+            UC7.ComputeWage();
+        }
+    }
+}
